f9ef5a70b47a752ec9817470763228e68c8f791a
emresen20 <emrebernarn@gmail.com>
2024-10-30 16:07:19 +0300

    courses page amd postman

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -3,5 +3,6 @@ const courseController = require("../controllers/courseController");
 
 const router = express.Router();
 router.route("/").post(courseController.createCourse); // localhost/courses yapılacak post request çalıştırır
+router.route('/').get(courseController.getAllCourses)
 
 module.exports = router;
