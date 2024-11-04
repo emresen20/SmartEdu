85eb7497b3af104af60d2039b12881225c6e0495
emresen20 <emrebernarn@gmail.com>
2024-11-04 21:40:42 +0300

    conncet mongo and logout

--- app.js
+++ app.js
@@ -1,49 +1,50 @@
 const express = require("express");
-const mongoose= require('mongoose')
-const pageRoute=require('./routes/pageRoute')
-const courseRoute=require('./routes/courseRoute');
-const categoryRoute=require('./routes/categoryRoute');
-const userRoute=require('./routes/userRoute');
-const session=require('express-session')
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 
 //Coonect DB
-mongoose.connect('mongodb://localhost/smartedu-db')
-  .then(() => console.log('Connected!'));
+mongoose
+  .connect("mongodb://localhost/smartedu-db")
+  .then(() => console.log("Connected!"));
 
-
-
-//Temlate Engine 
+//Temlate Engine
 app.set("view engine", "ejs");
 
 //Global Variable
 
-global.userIN= null; // global olarak bütün heryerde gözükmesini sağladık
+global.userIN = null; // global olarak bütün heryerde gözükmesini sağladık
 
 //Middlewares
 
-app.use(express.static("public"))
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true }))
-app.use(session({
-  secret: 'my_keyboard_cat',
-  resave: false,
-  saveUninitialized: true,
-})) 
-
-
+app.use(express.static("public"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "my_keyboard_cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }) //mongoya kayıt loglarını kaydediyor sunucuyu aç kapa yapsak bile çıkmıyor
+  })
+);
 
 //Routes
-app.use('*',(req,res,next)=>{ // burada bütün değişkelnlere bunu yollamış oldulk
-  userIN=req.session.userID; // giriş çıkış işlemlerinde hangi kullanıcı girdi çıktı authta userId yi düzenledik
+app.use("*", (req, res, next) => {
+  // burada bütün değişkelnlere bunu yollamış oldulk
+  userIN = req.session.userID; // giriş çıkış işlemlerinde hangi kullanıcı girdi çıktı authta userId yi düzenledik
   next();
-})
-app.use("/",pageRoute );
-app.use("/courses",courseRoute );
-app.use("/categories",categoryRoute)
-app.use("/users",userRoute)
-
+});
+app.use("/", pageRoute);
+app.use("/courses", courseRoute);
+app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
 const port = 3000;
 app.listen(port, () => {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 router.route("/signup").post(authController.createUser); // localhost/users/signup yapılacak post request çalıştırır
 router.route("/login").post(authController.loginUser); //localhost/users/login yapılacak post işlemi login ejs de actionda
+router.route("/logout").get(authController.logoutUser)
 
 module.exports = router;
