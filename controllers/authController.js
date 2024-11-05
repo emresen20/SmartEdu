4b8339231ed6589882ce368baa25e6aead95a3d1
emresen20 <emrebernarn@gmail.com>
2024-11-05 23:54:54 +0300

    courses create page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
-const bcrypt= require('bcrypt')
+const bcrypt= require('bcrypt');
+const Category=require('../models/Category')
 
 exports.createUser = async (req, res) => {
   try {
@@ -53,8 +54,11 @@ exports.loginUser = async (req, res) => {
 
   exports.getDashboardPage =  async (req, res) => {
      const  user=await User.findOne({_id:req.session.userID})
+     const categories= await Category.find();
+
     res.status(200).render("dashboard", {
       page_name: "dashboard",
-      user:user
+      user:user,
+      categories:categories
     });
   };
