0f0094a9a9495daa5c4c694d443d6123631f0416
emresen20 <emrebernarn@gmail.com>
2024-11-07 21:14:00 +0300

    enroll and release

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -79,10 +79,11 @@ exports.getAllCourses = async (req, res) => {
   exports.getCourses = async (req, res) => {
     try {
       const course = await Course.findOne({slug:req.params.slug}).populate('user'); // populate ederek user bilgilerinmi de almış olduk
-  
+      const user = await User.findById(req.session.userID)
       res.status(200).render('course-single',{
         course:course,
         page_name: "courses",
+        user
       })
     } catch(error) {
       res.status(400).json({
@@ -106,4 +107,20 @@ exports.getAllCourses = async (req, res) => {
       });
     }
   };
+
+  exports.releaseCourse = async (req, res) => {
+    try {
+    const user= await User.findById(req.session.userID);
+    await user.courses.pull({_id:req.body.course_id})
+    await user.save();
+  
+      res.status(200).redirect('/users/dashboard')
+    } catch(error) {
+      res.status(400).json({
+        status: "faild",
+        error,
+      });
+    }
+  };
+
 
