c3a0c9a25f93bc65ffe136c4ef7a6189553dce26
emresen20 <emrebernarn@gmail.com>
2024-11-08 20:47:37 +0300

    Serach process

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,6 +1,7 @@
 const Course = require("../models/Course");
 const Category = require("../models/Category");
 const User = require("../models/User");
+const { name } = require("ejs");
 
 exports.createCourse = async (req, res) => {
   try {
@@ -23,6 +24,7 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
+    const query = req.query.search;
     const category = await Category.findOne({ slug: categorySlug });
     let filter = {};
 
@@ -33,7 +35,22 @@ exports.getAllCourses = async (req, res) => {
       }
     }
 
-    const courses = await Course.find(filter).sort({ createdAt: -1 }).populate('user'); // Filtreyi burada uygular
+    if(query){
+      filter={name:query}
+    }
+
+    if (!query && !categorySlug){
+      filter.name= "",
+      filter.category= null
+    }
+
+
+    const courses = await Course.find({
+      $or:[
+        { name: {$regex: '.*' + filter.name + '.*' , $options:'i'}},
+        {category:filter.category}
+      ]
+    }).sort({ createdAt: -1 }).populate('user'); // Filtreyi burada uygular
     const categories = await Category.find(); // Tüm kategorileri getirir
 
     res.status(200).render("courses", {
@@ -80,10 +97,12 @@ exports.getAllCourses = async (req, res) => {
     try {
       const course = await Course.findOne({slug:req.params.slug}).populate('user'); // populate ederek user bilgilerinmi de almış olduk
       const user = await User.findById(req.session.userID)
+      const categories = await Category.find();
       res.status(200).render('course-single',{
         course:course,
         page_name: "courses",
-        user
+        user,
+        categories
       })
     } catch(error) {
       res.status(400).json({
